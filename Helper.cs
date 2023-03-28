fe0b9151258fbbcc24f162cf6f9439fe69329031
Justin Keiley <62526116+AnTic054@users.noreply.github.com>
2023-03-28 15:04:58 -0700

    Update Helper.cs

--- Helper.cs
+++ Helper.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Helper
 {
-
+    //This class is intended to be expanded upon with more helpful and simple functions to help clean up code
+    //in other scrpits and reduce how much rewriting needs to be done
     public float RandomFloatGen(float min, float max)
     {
         float newRandom;
