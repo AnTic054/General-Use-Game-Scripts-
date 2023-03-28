7cb949801b25d091885ea23fb8f92bfdbceb5744
Justin Keiley <62526116+AnTic054@users.noreply.github.com>
2023-03-28 15:02:41 -0700

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper
+{
+
+    public float RandomFloatGen(float min, float max)
+    {
+        float newRandom;
+        newRandom = Random.Range(min, max);
+
+        float RandomFloatGen = newRandom;
+        return RandomFloatGen;
+    }
+    public int RandomIntGen(int min, int max)
+    {
+        int newRandom;
+        newRandom = Random.Range(min, max);
+
+        int RandomIntGen = newRandom;
+        return RandomIntGen;
+    }
+}
